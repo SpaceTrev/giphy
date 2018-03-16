867b6cf51269d4f61411d86ca2c345c234910d1d
Trevor Benavides <trevorbenavides@Trevors-MacBook-Pro.local>
2018-03-16 13:22:52 -0600

    works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,18 +12,26 @@ function createButtons() {
 }
 function displayGif() {
     var gifName = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=UIcheBD4aNnGl9qzv89JH5tOKQIsHky3&limit=5";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=UIcheBD4aNnGl9qzv89JH5tOKQIsHky3&limit=5&fmt";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
-        var imageURL = response.data[0].url;
+        var imageURL = response.data[0].images.fixed_height.url;
         var gifImage = $("<img>");
         var gifRating = response.data[0].rating;
         gifImage.attr("src", imageURL);
-        $("#gifImage").prepend(gifImage);
-        $("#gifImage").append("Rating: " + gifRating);
+        $("#gifImage").append(gifImage);
+        $("#gifImage").append("Rating: " + gifRating + "<br>");
         console.log(response.data[0]);
+        // $("#gifImage").on("click", function(){
+        //     if (imageURL === true) {
+        //         var movingURL = response.data[0].images.fixed_height.url
+        //         $("#gifImage", "src").empty().attr("src", movingURL);
+        //         console.log(click);
+        //     }
+
+        // })
     });
       
 
